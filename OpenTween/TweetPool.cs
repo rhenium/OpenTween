96b988e5cd79f024ea44e77223c6eb2e81c65ef0
alicesanten <santen3ten@yahoo.co.jp>
2012-09-27 20:58:34 +0900

    規制時自動アカウント切り替え＋ポスト

--- OpenTween/TweetPool.cs
+++ OpenTween/TweetPool.cs
@@ -87,5 +87,14 @@ namespace OpenTween
                 textBoxTweet.SelectAll();
             }
         }
+        
+        private void auto_buttonPost_Click(object sender, EventArgs e)
+        {
+            SendKeys.SendWait("^+A");
+            System.Threading.Thread.Sleep(10);
+            Thread t = new Thread(new ParameterizedThreadStart(worker));
+            t.Start(new object[] { this.textBoxTweet.Text, this.reply_to, this.mediaFile });
+            this.Close();
+        }
     }
 }
