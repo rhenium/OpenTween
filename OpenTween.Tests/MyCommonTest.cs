2c3f8dabb37251f43406457695c2fe43123f3264
re4k <re4k@re4k.info>
2012-12-25 00:51:40 +0900

    OpenTween.MyCommon.GetReadableVersionのテスト削除

--- OpenTween.Tests/MyCommonTest.cs
+++ OpenTween.Tests/MyCommonTest.cs
@@ -151,18 +151,6 @@ namespace OpenTween
             Assert.That(MyCommon.ReplaceAppName(str, "OpenTween"), Is.EqualTo(except));
         }
 
-        [TestCase("1.0.0.0", "1.0.0")]
-        [TestCase("1.0.0.1", "1.0.1-beta1")]
-        [TestCase("1.0.0.9", "1.0.1-beta9")]
-        [TestCase("1.0.1.0", "1.0.1")]
-        [TestCase("1.0.9.1", "1.1.0-beta1")]
-        [TestCase("1.1.0.0", "1.1.0")]
-        [TestCase("1.9.9.1", "2.0.0-beta1")]
-        public void GetReadableVersionTest(string fileVersion, string expect)
-        {
-            Assert.That(OpenTween.MyCommon.GetReadableVersion(fileVersion), Is.EqualTo(expect));
-        }
-
         public static object[] GetStatusUrlTest1_TestCase =
         {
             new object[] {
